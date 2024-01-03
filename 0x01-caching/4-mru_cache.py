06b66cc1b94e12c06ac905d9af118f439af2d005
Eliab E <eliaberango@gmail.com>
2024-01-03 04:22:47 -0800

    Add 4-mru_cache.py

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""MRUCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRUCache class
+    """
+    def __init__(self):
+        """Initialization
+        """
+        self.mru = []
+        super().__init__()
+
+    def put(self, key, item):
+        """Assign to the dictionary self.cache_data the item value
+           for the key key
+        Args:
+            key - a key
+            item - an item
+        """
+        if key is not None and item is not None:
+            ln = len(self.cache_data)
+            if ln >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.mru[-1]))
+                del self.cache_data[self.mru[-1]]
+                del self.mru[-1]
+            if key in self.cache_data:
+                del self.mru[self.mru.index(key)]
+            self.mru.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key
+        Args:
+            key - a key
+        Returns:
+            returns a value
+        """
+        if key is not None and key in self.cache_data.keys():
+            del self.mru[self.mru.index(key)]
+            self.mru.append(key)
+            return self.cache_data.get(key)
+        else:
+            return None
